1ab49e3f1864b2567905ccb652792ce7c622250c
D-Stap <Davantestapleton49@gmail.com>
2025-11-21 10:37:44 -0500

    canceled out test files and updated README

--- src/app.py
+++ src/app.py
@@ -1,8 +1,7 @@
 #import os
-''''
-#Hardcoded secret (insecure)
-API_KEY = "12345-abcde-SECRET"
 
-def handler(event, context):
-    print("Running insecure app...")
-'''
+#Hardcoded secret (insecure)
+#API_KEY = "12345-abcde-SECRET"
+
+#def handler(event, context):
+#    print("Running insecure app...")
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 
 #resource "aws_s3_bucket" "bad_example" {
- # bucket = "example-bucket-name"
+#  bucket = "example-bucket-name"
 #}
 
 #resource "aws_s3_bucket_acl" "bad_example" {
-  #bucket = aws_s3_bucket.bad_example.id
- # acl    = "public-read" # Insecure: public read access
+#  bucket = aws_s3_bucket.bad_example.id
+#  acl    = "public-read" # Insecure: public read access
 #}
