63cbc2447f6eeb38ad2ee797b829239db8a407f7
D-Stap <Davantestapleton49@gmail.com>
2025-11-21 10:14:41 -0500

    added test files

---
+++ terraform/main.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "bad_example" {
+  bucket = "example-bucket-name"
+}
+
+resource "aws_s3_bucket_acl" "bad_example" {
+  bucket = aws_s3_bucket.bad_example.id
+  acl    = "public-read"  # Insecure: public read access
+}
